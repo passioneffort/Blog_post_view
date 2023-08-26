9587525e270d88a07384c6860dae67d82789c793
passioneffort <agronkercishta.ai@gmail.com>
2023-08-25 20:34:35 -0400

    modify

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -7,7 +7,7 @@ const { createSecretToken } = require("../util/SecretToken");
 
 
 module.exports.Signup = async (req, res, next) => {
-  console.log(req.body);
+  // console.log(req.body);
   try {
     const { email, password, username, createdAt } = req.body;
     const existingUser = await User.findOne({ email });
@@ -57,7 +57,7 @@ module.exports.Login = async (req, res, next) => {
 }
 
 exports.createOneBlog = (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   AppBlog.create(req.body)
     .then((blog) => {
       console.log({ blog });
@@ -67,7 +67,6 @@ exports.createOneBlog = (req, res) => {
       });
     })
     .catch((err) => {
-      console.log('hhhhhhhhhhhhhhhhh');
       res.status(404).json({
         message: "Sorry your blog list cannot be added",
         error: err.message,
