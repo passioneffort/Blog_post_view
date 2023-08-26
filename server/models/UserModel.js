2fc6714ddf933984954fce0760e5a040d551f5a5
passioneffort <agronkercishta.ai@gmail.com>
2023-08-25 20:33:14 -0400

    modify

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Your password is required"],
   },
-  createdAt: {
+  date: {
     type: Date,
     default: new Date(),
   },
--- server/server.js
+++ server/server.js
@@ -1,39 +1,33 @@
-
 const express = require("express");
 const cors = require("cors");
 const configDatabase = require("./configurations/database.js");
 const route = require("./routes/routes.js");
-// const route = require("./routes/AuthRoute.js");
+const bodyParser = require("body-parser");
 
 const dotenv = require("dotenv");
-
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 configDatabase();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
 app.use(cors({ origin: true, credentials: true }));
-
 app.use(express.json({ extended: false }));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
 
-
-
-
-//connecting to the mongodb database
-
-
-// add the middlewares
 app.get("/", (req, res) =>
   res.send("Hello there!! Cheers !! The server is up and running")
 );
 
 // using our routes
 app.use("/", route);
-
-
 
 // listen
 app.listen(PORT, () =>
