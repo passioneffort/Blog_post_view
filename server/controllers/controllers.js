05f1f0cf0f9856b1b9efd79bb5819c257a03201e
passioneffort <agronkercishta.ai@gmail.com>
2023-08-26 21:21:06 -0400

    modify

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -56,37 +56,37 @@ module.exports.Login = async (req, res, next) => {
   }
 }
 
-// exports.createOneBlog = (req, res) => {
-//   // console.log(req.body);
-//   AppBlog.create(req.body)
-//     .then((blog) => {
-//       console.log({ blog });
-//       res.json({
-//         message: "Cheers!! You have successfully added Blog",
-//         blog,
-//       });
-//     })
-//     .catch((err) => {
-//       res.status(404).json({
-//         message: "Sorry your blog list cannot be added",
-//         error: err.message,
-//       });
-//     });
-// };
+exports.createOneBlog = (req, res) => {
+  // console.log(req.body);
+  AppBlog.create(req.body)
+    .then((blog) => {
+      console.log({ blog });
+      res.json({
+        message: "Cheers!! You have successfully added Blog",
+        blog,
+      });
+    })
+    .catch((err) => {
+      res.status(404).json({
+        message: "Sorry your blog list cannot be added",
+        error: err.message,
+      });
+    });
+};
 
-// exports.listAllBlog = (req, res) => {
-//   AppBlog.find()
-//     .then((blog) => {
-//       console.log({ blog });
-//       res.json(blog);
-//     })
-//     .catch((err) => {
-//       res.status(404).json({ 
-//         message: "There isnt any blog available", 
-//         error: err.message,
-//       });
-//     });
-// };
+exports.listAllBlog = (req, res) => {
+  AppBlog.find()
+    .then((blog) => {
+      console.log({ blog });
+      res.json(blog);
+    })
+    .catch((err) => {
+      res.status(404).json({ 
+        message: "There isnt any blog available", 
+        error: err.message,
+      });
+    });
+};
 
 exports.updateOneBlog = (req, res) => {
   AppBlog.findByIdAndUpdate(req.params.id, req.body)
