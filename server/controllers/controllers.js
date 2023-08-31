c75f1099de9cf6433339695869a26879a394c365
passioneffort <agronkercishta.ai@gmail.com>
2023-08-31 16:30:23 -0400

    modify

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -6,29 +6,29 @@ const { createSecretToken } = require("../util/SecretToken");
 // const bcrypt = require("bcryptjs");
 
 
-// module.exports.Signup = async (req, res, next) => {
-//   // console.log(req.body);
-//   try {
-//     const { email, password, username, createdAt } = req.body;
-//     const existingUser = await User.findOne({ email });
+module.exports.Signup = async (req, res, next) => {
+  // console.log(req.body);
+  try {
+    const { email, password, username, createdAt } = req.body;
+    const existingUser = await User.findOne({ email });
     
-//     if (existingUser) {
-//       return res.json({ message: "User already exists" });
-//     }
-//     const user = await User.create({ email, password, username, createdAt });
-//     const token = createSecretToken(user._id);
-//     res.cookie("token", token, {
-//       withCredentials: true,
-//       httpOnly: false,
-//     });
-//     res
-//       .status(201)
-//       .json({ message: "User signed in successfully", success: true, user });
-//     next();
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+    if (existingUser) {
+      return res.json({ message: "User already exists" });
+    }
+    const user = await User.create({ email, password, username, createdAt });
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User signed in successfully", success: true, user });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports.Login = async (req, res, next) => {
   try {
@@ -58,23 +58,23 @@ module.exports.Login = async (req, res, next) => {
   }
 }
 
-exports.createOneBlog = (req, res) => {
-  // console.log(req.body);
-  AppBlog.create(req.body)
-    .then((blog) => {
-      console.log({ blog });
-      res.json({
-        message: "Cheers!! You have successfully added Blog",
-        blog,
-      });
-    })
-    .catch((err) => {
-      res.status(404).json({
-        message: "Sorry your blog list cannot be added",
-        error: err.message,
-      });
-    });
-};
+// exports.createOneBlog = (req, res) => {
+//   // console.log(req.body);
+//   AppBlog.create(req.body)
+//     .then((blog) => {
+//       console.log({ blog });
+//       res.json({
+//         message: "Cheers!! You have successfully added Blog",
+//         blog,
+//       });
+//     })
+//     .catch((err) => {
+//       res.status(404).json({
+//         message: "Sorry your blog list cannot be added",
+//         error: err.message,
+//       });
+//     });
+// };
 
 exports.listAllBlog = (req, res) => {
   AppBlog.find()
