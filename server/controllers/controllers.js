2b9303d1fe05db83337c35edc9042bd531341882
passioneffort <agronkercishta.ai@gmail.com>
2023-09-01 13:26:42 -0400

    modify

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -33,16 +33,16 @@ module.exports.Signup = async (req, res, next) => {
 module.exports.Login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    // if(!email || !password ){
-    //   return res.json({message:'All fields are required'})
-    // }
-    // const user = await User.findOne({ email });
-    // if(!user){
-    //   return res.json({message:'Incorrect password or email' }) 
-    // }
-    // const auth = await bcrypt.compare(password,user.password)
-    // console.log("~~~~~~~~~~~~~~");
-    // console.log(auth);
+    if(!email || !password ){
+      return res.json({message:'All fields are required'})
+    }
+    const user = await User.findOne({ email });
+    if(!user){
+      return res.json({message:'Incorrect password or email' }) 
+    }
+    const auth = await bcrypt.compare(password,user.password)
+    console.log("~~~~~~~~~~~~~~");
+    console.log(auth);
     if (!auth) {
       return res.json({message:'Incorrect password or email' }) 
     }
