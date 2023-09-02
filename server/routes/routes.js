bb84d706b5780b3a617ff920a1bc39ac72db227e
passioneffort <agronkercishta.ai@gmail.com>
2023-09-02 19:37:36 -0400

    modify

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -11,11 +11,11 @@ const {
   deleteBlog,
 } = require("../controllers/controllers.js");
 
-router.post("/signup", Signup);
-router.post("/login", Login);
-router.get("/list/", listAllBlog);
-router.post("/create", createOneBlog);
-router.put("/update/:id", updateOneBlog);
-router.delete("/delete/:id", deleteBlog);
+// router.post("/signup", Signup);
+// router.post("/login", Login);
+// router.get("/list/", listAllBlog);
+// router.post("/create", createOneBlog);
+// router.put("/update/:id", updateOneBlog);
+// router.delete("/delete/:id", deleteBlog);
 
 module.exports = router;
